b8ddaf1e62ebeb7022e5fd54ca332f4ab6b32ca2
azzumwaqar <azzumwaqar@gmail.com>
2023-07-26 18:37:53 +0100

    chore: ui package added

--- app/src/main/java/com/example/hoxtonprayertimeapp/ui/prayer/PrayerFragment.kt
+++ app/src/main/java/com/example/hoxtonprayertimeapp/ui/prayer/PrayerFragment.kt
@@ -1,4 +1,4 @@
-package com.example.hoxtonprayertimeapp.prayer
+package com.example.hoxtonprayertimeapp.ui.prayer
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/example/hoxtonprayertimeapp/ui/prayer/PrayerViewModel.kt
+++ app/src/main/java/com/example/hoxtonprayertimeapp/ui/prayer/PrayerViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.hoxtonprayertimeapp.prayer
+package com.example.hoxtonprayertimeapp.ui.prayer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
