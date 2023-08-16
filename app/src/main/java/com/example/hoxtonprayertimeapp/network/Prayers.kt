2f2cc836cbab687c9884e968fef49cd664e6debe
azzumwaqar <azzumwaqar@gmail.com>
2023-08-16 13:07:04 +0100

    feat: PrayersApiService.kt added

---
+++ app/src/main/java/com/example/hoxtonprayertimeapp/network/Prayers.kt
@@ -0,0 +1,11 @@
+package com.example.hoxtonprayertimeapp.network
+
+data class Prayers(
+    val date:String,
+    val fajr:String,
+    val sunrise:String,
+    val dhuhr:String,
+    val asr:String,
+    val magrib:String,
+    val isha:String
+)
---
+++ app/src/main/java/com/example/hoxtonprayertimeapp/network/PrayersApiService.kt
@@ -0,0 +1,32 @@
+package com.example.hoxtonprayertimeapp.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Query
+
+
+const val KEY = "f149aaef-856d-421a-8e1c-0b2f80c11174"
+private const val BASE_URL = "https://www.londonprayertimes.com/api/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface PrayersApiService{
+    @GET("times")
+    suspend fun getTodaysTimes(@Query("format") format:String,@Query("key") key:String):Prayers
+}
+
+object PrayersApi{
+    val retrofitService : PrayersApiService by lazy {
+        retrofit.create(PrayersApiService::class.java)
+    }
+}
