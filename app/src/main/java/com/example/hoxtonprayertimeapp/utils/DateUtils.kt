a57e1b4f37ee7f4c19906c42cdb4f988bcc14cce
azzumwaqar <azzumwaqar@gmail.com>
2023-08-16 20:20:11 +0100

    fix: Maghrib Next Jamaat text too big for the text view. reduced to 40sp.

--- app/src/main/java/com/example/hoxtonprayertimeapp/utils/DateUtils.kt
+++ app/src/main/java/com/example/hoxtonprayertimeapp/utils/DateUtils.kt
@@ -99,11 +99,12 @@ fun createDocumentReferenceIDForLastWeek(calender: java.util.Calendar) = getLast
 fun isFridayToday() = java.util.Calendar.getInstance(Locale.getDefault()).get(java.util.Calendar.DAY_OF_WEEK) == java.util.Calendar.FRIDAY
 
 fun formatTimeToString(time: Date?): String? {
-    val formatter = SimpleDateFormat("hh:mm a")
+    val formatter = SimpleDateFormat("hh:mma")
     if (time != null){
         val formattedTime = formatter.format(time)
         Log.e("formatStringTime",formattedTime)
-        return formattedTime
+
+        return  formattedTime.lowercase()
     }
 //    val date = formatter.parse(time)
    return null
