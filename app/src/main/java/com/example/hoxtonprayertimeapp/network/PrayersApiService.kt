94bc1e6bc1dc38a8fb1cb52e9e0b5f45f5bba138
azzumwaqar <azzumwaqar@gmail.com>
2023-10-12 13:56:14 +0100

    chore: cleanup - secrets lib added

--- app/src/main/java/com/example/hoxtonprayertimeapp/network/PrayersApiService.kt
+++ app/src/main/java/com/example/hoxtonprayertimeapp/network/PrayersApiService.kt
@@ -1,6 +1,7 @@
 package com.example.hoxtonprayertimeapp.network
 
 import com.example.hoxtonprayertimeapp.models.LondonPrayersBeginningTimes
+import com.hoxtonislah.hoxtonprayertimeapp.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
@@ -9,7 +10,6 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Query
 
 const val QUERY_FORMAT_JSON = "json"
-const val KEY = "f149aaef-856d-421a-8e1c-0b2f80c11174"
 private const val BASE_URL = "https://www.londonprayertimes.com/api/"
 
 private val moshi = Moshi.Builder()
@@ -25,7 +25,7 @@ interface PrayersApiService {
     @GET("times")
     suspend fun getTodaysPrayerBeginningTimes(
         @Query("format") format: String = QUERY_FORMAT_JSON,
-        @Query("key") key: String = KEY,
+        @Query("key") key: String = BuildConfig.londonPrayerApiKey,
         @Query("24hours")timeFormat : Boolean =  true,
         @Query("date") date: String
     ): LondonPrayersBeginningTimes
